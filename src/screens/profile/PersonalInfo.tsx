bb1829138fca574472c7c781950907988b2432af
nntuyen1505 <nntuyen1505@gmail.com>
2024-11-16 21:31:33 +0700

    test add husky

--- src/screens/profile/PersonalInfo.tsx
+++ src/screens/profile/PersonalInfo.tsx
@@ -7,96 +7,108 @@ import {appColors} from '../../constants';
 import {useAuthStore} from '../../stores/auth.store';
 
 const PersonalInfo = ({navigation}: any) => {
-  const auth = useAuthStore(state => state.auth);
-  console.log(auth);
-  return (
-    <SectionComponent styles={{flex: 1, backgroundColor: 'white'}}>
-      <SpaceComponent height={70} />
-      <View style={{alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row'}}>
-        <BackHeader btn="gray" title={'Personal Infor'} />
+    const auth = useAuthStore(state => state.auth);
+    //   console.log(auth);
+    return (
+        <SectionComponent styles={{flex: 1, backgroundColor: 'white'}}>
+            <SpaceComponent height={70} />
+            <View
+                style={{
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    flexDirection: 'row',
+                }}>
+                <BackHeader btn="gray" title={'Personal Infor'} />
 
-        <ButtonComponent
-          type="link"
-          text="Edit"
-          icon={ICMore}
-          onPress={() => {
-            navigation.navigate('EditProfile');
-          }}
-        />
-      </View>
-      <SpaceComponent height={24} />
+                <ButtonComponent
+                    type="link"
+                    text="Edit"
+                    icon={ICMore}
+                    onPress={() => {
+                        navigation.navigate('EditProfile');
+                    }}
+                />
+            </View>
+            <SpaceComponent height={24} />
 
-      {/* AVATAR */}
-      <View>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'flex-start',
-            gap: 32,
-          }}>
-          <Image source={{uri: auth?.image}} style={{width: 100, height: 100, borderRadius: 50}} />
-          <View style={{alignItems: 'flex-start', justifyContent: 'center'}}>
-            <TextComponent
-              font="Poppins-Bold"
-              text={auth?.firstName + ' ' + auth?.lastName}
-              styles={{
-                fontSize: 20,
-                fontFamily: 'Poppins-Bold',
-                color: appColors.text,
-                textAlign: 'center',
-              }}
-            />
-            <TextComponent font="Poppins-Bold" text="I love fast food" color={appColors.placeholderColor} />
-          </View>
-        </View>
-      </View>
-      <SpaceComponent height={32} />
-      {/* Component Tab */}
-      <View style={styles.boxBackground}>
-        <SpaceComponent height={20} />
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('PersonalInfo');
-          }}>
-          <SectionComponent styles={styles.flexCenterContainer}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <ICPersonalInfo />
-              <SpaceComponent width={8} />
-              <View>
-                <TextComponent font="Poppins-Bold" text="FULL NAME" />
-                <TextComponent text={auth?.firstName + ' ' + auth?.lastName} />
-              </View>
+            {/* AVATAR */}
+            <View>
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'flex-start',
+                        gap: 32,
+                    }}>
+                    <Image
+                        source={{uri: auth?.image}}
+                        style={{width: 100, height: 100, borderRadius: 50}}
+                    />
+                    <View style={{alignItems: 'flex-start', justifyContent: 'center'}}>
+                        <TextComponent
+                            font="Poppins-Bold"
+                            text={auth?.firstName + ' ' + auth?.lastName}
+                            styles={{
+                                fontSize: 20,
+                                fontFamily: 'Poppins-Bold',
+                                color: appColors.text,
+                                textAlign: 'center',
+                            }}
+                        />
+                        <TextComponent
+                            font="Poppins-Bold"
+                            text="I love fast food"
+                            color={appColors.placeholderColor}
+                        />
+                    </View>
+                </View>
             </View>
-          </SectionComponent>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {}}>
-          <SectionComponent styles={styles.flexCenterContainer}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <ICEmailProfile />
-              <SpaceComponent width={8} />
-              <View>
-                <TextComponent font="Poppins-Bold" text="EMAIL" />
-                <TextComponent text={auth?.email} />
-              </View>
+            <SpaceComponent height={32} />
+            {/* Component Tab */}
+            <View style={styles.boxBackground}>
+                <SpaceComponent height={20} />
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate('PersonalInfo');
+                    }}>
+                    <SectionComponent styles={styles.flexCenterContainer}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <ICPersonalInfo />
+                            <SpaceComponent width={8} />
+                            <View>
+                                <TextComponent font="Poppins-Bold" text="FULL NAME" />
+                                <TextComponent text={auth?.firstName + ' ' + auth?.lastName} />
+                            </View>
+                        </View>
+                    </SectionComponent>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {}}>
+                    <SectionComponent styles={styles.flexCenterContainer}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <ICEmailProfile />
+                            <SpaceComponent width={8} />
+                            <View>
+                                <TextComponent font="Poppins-Bold" text="EMAIL" />
+                                <TextComponent text={auth?.email} />
+                            </View>
+                        </View>
+                    </SectionComponent>
+                </TouchableOpacity>
             </View>
-          </SectionComponent>
-        </TouchableOpacity>
-      </View>
-    </SectionComponent>
-  );
+        </SectionComponent>
+    );
 };
 
 export default PersonalInfo;
 
 const styles = StyleSheet.create({
-  boxBackground: {
-    backgroundColor: appColors.background1,
-    borderRadius: 16,
-  },
-  flexCenterContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
+    boxBackground: {
+        backgroundColor: appColors.background1,
+        borderRadius: 16,
+    },
+    flexCenterContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
 });
--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,18 +1,18 @@
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import React from 'react';
 import {
-  ICAddress,
-  ICBackWhite,
-  ICCart,
-  ICCartProfile,
-  ICChevronRight,
-  ICFAQs,
-  ICFavouriteProfile,
-  ICLogOut,
-  ICMore,
-  ICNotificationProfile,
-  ICPersonalInfo,
-  ICSettings,
+    ICAddress,
+    ICBackWhite,
+    ICCart,
+    ICCartProfile,
+    ICChevronRight,
+    ICFAQs,
+    ICFavouriteProfile,
+    ICLogOut,
+    ICMore,
+    ICNotificationProfile,
+    ICPersonalInfo,
+    ICSettings,
 } from '../../assets/svgs';
 import BackHeader from '../../components/Header/BackHeader';
 import {SectionComponent, SpaceComponent, TextComponent} from '../../components';
@@ -23,169 +23,182 @@ import {ArrowRotateLeft, RotateRight} from 'iconsax-react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ProfileScreen = ({navigation}: any) => {
-  const auth = useAuthStore(state => state.auth);
-  const logout = useAuthStore(state => state.logout);
+    const auth = useAuthStore(state => state.auth);
+    console.log('auth :>> ', auth);
+    const logout = useAuthStore(state => state.logout);
 
-  return (
-    <SectionComponent styles={{flex: 1, backgroundColor: 'white'}}>
-      <SpaceComponent height={70} />
-      <View style={{alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row'}}>
-        <BackHeader btn="gray" title={'Profile'} />
+    return (
+        <SectionComponent styles={{flex: 1, backgroundColor: 'white'}}>
+            <SpaceComponent height={70} />
+            <View
+                style={{
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    flexDirection: 'row',
+                }}>
+                <BackHeader btn="gray" title={'Profile'} />
 
-        <TouchableOpacity onPress={() => {}}>
-          <ICMore />
-        </TouchableOpacity>
-      </View>
-      <SpaceComponent height={24} />
-      {/* AVATAR */}
-      <View>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'flex-start',
-            gap: 32,
-          }}>
-          <Image source={{uri: auth?.image}} style={{width: 100, height: 100, borderRadius: 50}} />
-          <View style={{alignItems: 'flex-start', justifyContent: 'center'}}>
-            <TextComponent
-              font="Poppins-Bold"
-              text={auth?.firstName + ' ' + auth?.lastName}
-              styles={{
-                fontSize: 20,
-                fontFamily: 'Poppins-Bold',
-                color: appColors.text,
-                textAlign: 'center',
-              }}
-            />
-            <SpaceComponent height={8} />
-            <TextComponent font="Poppins-Bold" text="I love fast food" color={appColors.placeholderColor} />
-          </View>
-        </View>
-      </View>
-      <SpaceComponent height={32} />
-      {/* Component Tab 1*/}
-      <View style={styles.boxBackground}>
-        <SpaceComponent height={20} />
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('PersonalInfo');
-          }}>
-          <SectionComponent styles={styles.flexCenterContainer}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <ICPersonalInfo />
-              <SpaceComponent width={8} />
-              <TextComponent font="Poppins-Bold" text="Personal Info" />
+                <TouchableOpacity onPress={() => {}}>
+                    <ICMore />
+                </TouchableOpacity>
             </View>
-            <ICChevronRight />
-          </SectionComponent>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {}}>
-          <SectionComponent styles={styles.flexCenterContainer}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <ICAddress />
-              <SpaceComponent width={8} />
-              <TextComponent font="Poppins-Bold" text="Address" />
+            <SpaceComponent height={24} />
+            {/* AVATAR */}
+            <View>
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'flex-start',
+                        gap: 32,
+                    }}>
+                    <Image
+                        source={{uri: auth?.image}}
+                        style={{width: 100, height: 100, borderRadius: 50}}
+                    />
+                    <View style={{alignItems: 'flex-start', justifyContent: 'center'}}>
+                        <TextComponent
+                            font="Poppins-Bold"
+                            text={auth?.firstName + ' ' + auth?.lastName}
+                            styles={{
+                                fontSize: 20,
+                                fontFamily: 'Poppins-Bold',
+                                color: appColors.text,
+                                textAlign: 'center',
+                            }}
+                        />
+                        <SpaceComponent height={8} />
+                        <TextComponent
+                            font="Poppins-Bold"
+                            text="I love fast food"
+                            color={appColors.placeholderColor}
+                        />
+                    </View>
+                </View>
             </View>
-            <ICChevronRight />
-          </SectionComponent>
-        </TouchableOpacity>
-      </View>
-      <SpaceComponent height={20} />
-      {/* Component Tab 2 */}
-      <View style={styles.boxBackground}>
-        <SpaceComponent height={20} />
-        <TouchableOpacity onPress={() => {}}>
-          <SectionComponent styles={styles.flexCenterContainer}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <ICCartProfile />
-              <SpaceComponent width={8} />
-              <TextComponent font="Poppins-Bold" text="Cart" />
+            <SpaceComponent height={32} />
+            {/* Component Tab 1*/}
+            <View style={styles.boxBackground}>
+                <SpaceComponent height={20} />
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate('PersonalInfo');
+                    }}>
+                    <SectionComponent styles={styles.flexCenterContainer}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <ICPersonalInfo />
+                            <SpaceComponent width={8} />
+                            <TextComponent font="Poppins-Bold" text="Personal Info" />
+                        </View>
+                        <ICChevronRight />
+                    </SectionComponent>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {}}>
+                    <SectionComponent styles={styles.flexCenterContainer}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <ICAddress />
+                            <SpaceComponent width={8} />
+                            <TextComponent font="Poppins-Bold" text="Address" />
+                        </View>
+                        <ICChevronRight />
+                    </SectionComponent>
+                </TouchableOpacity>
             </View>
-            <ICChevronRight />
-          </SectionComponent>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {}}>
-          <SectionComponent styles={styles.flexCenterContainer}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <ICFavouriteProfile />
-              <SpaceComponent width={8} />
-              <TextComponent font="Poppins-Bold" text="Favourite" />
+            <SpaceComponent height={20} />
+            {/* Component Tab 2 */}
+            <View style={styles.boxBackground}>
+                <SpaceComponent height={20} />
+                <TouchableOpacity onPress={() => {}}>
+                    <SectionComponent styles={styles.flexCenterContainer}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <ICCartProfile />
+                            <SpaceComponent width={8} />
+                            <TextComponent font="Poppins-Bold" text="Cart" />
+                        </View>
+                        <ICChevronRight />
+                    </SectionComponent>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {}}>
+                    <SectionComponent styles={styles.flexCenterContainer}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <ICFavouriteProfile />
+                            <SpaceComponent width={8} />
+                            <TextComponent font="Poppins-Bold" text="Favourite" />
+                        </View>
+                        <ICChevronRight />
+                    </SectionComponent>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {}}>
+                    <SectionComponent styles={styles.flexCenterContainer}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <ICNotificationProfile />
+                            <SpaceComponent width={8} />
+                            <TextComponent font="Poppins-Bold" text="Notifications" />
+                        </View>
+                        <ICChevronRight />
+                    </SectionComponent>
+                </TouchableOpacity>
             </View>
-            <ICChevronRight />
-          </SectionComponent>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {}}>
-          <SectionComponent styles={styles.flexCenterContainer}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <ICNotificationProfile />
-              <SpaceComponent width={8} />
-              <TextComponent font="Poppins-Bold" text="Notifications" />
-            </View>
-            <ICChevronRight />
-          </SectionComponent>
-        </TouchableOpacity>
-      </View>
-      <SpaceComponent height={20} />
+            <SpaceComponent height={20} />
 
-      {/* Component Tab 3 */}
-      <View style={styles.boxBackground}>
-        <SpaceComponent height={20} />
-        <TouchableOpacity onPress={() => {}}>
-          <SectionComponent styles={styles.flexCenterContainer}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <ICFAQs />
-              <SpaceComponent width={8} />
-              <TextComponent font="Poppins-Bold" text="FAQs" />
+            {/* Component Tab 3 */}
+            <View style={styles.boxBackground}>
+                <SpaceComponent height={20} />
+                <TouchableOpacity onPress={() => {}}>
+                    <SectionComponent styles={styles.flexCenterContainer}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <ICFAQs />
+                            <SpaceComponent width={8} />
+                            <TextComponent font="Poppins-Bold" text="FAQs" />
+                        </View>
+                        <ICChevronRight />
+                    </SectionComponent>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {}}>
+                    <SectionComponent styles={styles.flexCenterContainer}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <ICSettings />
+                            <SpaceComponent width={8} />
+                            <TextComponent font="Poppins-Bold" text="Settings" />
+                        </View>
+                        <ICChevronRight />
+                    </SectionComponent>
+                </TouchableOpacity>
             </View>
-            <ICChevronRight />
-          </SectionComponent>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {}}>
-          <SectionComponent styles={styles.flexCenterContainer}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <ICSettings />
-              <SpaceComponent width={8} />
-              <TextComponent font="Poppins-Bold" text="Settings" />
-            </View>
-            <ICChevronRight />
-          </SectionComponent>
-        </TouchableOpacity>
-      </View>
-      <SpaceComponent height={20} />
+            <SpaceComponent height={20} />
 
-      {/* Component Tab 4 */}
-      <View style={styles.boxBackground}>
-        <SpaceComponent height={20} />
-        <TouchableOpacity
-          onPress={async () => {
-            logout();
-            await AsyncStorage.clear();
-          }}>
-          <SectionComponent styles={styles.flexCenterContainer}>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <ICLogOut />
-              <SpaceComponent width={8} />
-              <TextComponent font="Poppins-Bold" text="Log Out" />
+            {/* Component Tab 4 */}
+            <View style={styles.boxBackground}>
+                <SpaceComponent height={20} />
+                <TouchableOpacity
+                    onPress={async () => {
+                        logout();
+                        await AsyncStorage.clear();
+                    }}>
+                    <SectionComponent styles={styles.flexCenterContainer}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <ICLogOut />
+                            <SpaceComponent width={8} />
+                            <TextComponent font="Poppins-Bold" text="Log Out" />
+                        </View>
+                        <ICChevronRight />
+                    </SectionComponent>
+                </TouchableOpacity>
             </View>
-            <ICChevronRight />
-          </SectionComponent>
-        </TouchableOpacity>
-      </View>
-    </SectionComponent>
-  );
+        </SectionComponent>
+    );
 };
 
 export default ProfileScreen;
 
 const styles = StyleSheet.create({
-  boxBackground: {
-    backgroundColor: appColors.background1,
-    borderRadius: 16,
-  },
-  flexCenterContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
+    boxBackground: {
+        backgroundColor: appColors.background1,
+        borderRadius: 16,
+    },
+    flexCenterContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
 });
